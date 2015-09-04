6db0298a5861f0fe33378e1a2b7f8be750aa8fc3
Kieran O'Sullivan <kieran_nothing@hotmail.com>
2015-09-04 15:04:45 +0100

    Update login.php

--- login.php
+++ login.php
@@ -3,8 +3,8 @@ require "twitteroauth/autoload.php";
 use Abraham\TwitterOAuth\TwitterOAuth;
 session_start();
 
-$consumer_key = "rpDEdoWwo8Yv7hteea1rKwTHA";
-$consumer_secret = "N4fBrzBQy3ZyaH9kqTml4MZNNrCTiSzIVFl5BaATgyi4K4hrEb";
+$consumer_key = "******************";
+$consumer_secret = "*************************";
 $oauth_callback = 'http://127.0.0.1/Login/newcallback.php';
 $connection = new TwitterOAuth($consumer_key,$consumer_secret);
 $request_token = $connection->oauth('oauth/request_token', array('oauth_callback' => $oauth_callback));
